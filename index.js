c6957702385a7b4f113be83513bf2f68dc950021
G4be34 <jimenezgabriel12@gmail.com>
2023-01-14 21:03:05 -0700

    Add class names and button variables

---
+++ index.js
@@ -0,0 +1,12 @@
+class Calculator {
+    constructor(prevOutput, curOutput);
+};
+
+const numbers = document.querySelectorAll('.btnNum');
+const operations = document.querySelectorAll('.operationBtn');
+const allClear = document.querySelector('.acBtn');
+const delBtn = document.querySelector('.delBtn');
+const equals = document.querySelector('.equalsBtn');
+const prevOutput = document.querySelector('.previous-output');
+const curOutput = document.querySelector('.current-output');
+
