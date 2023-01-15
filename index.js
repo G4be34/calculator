3d42b3e8e4c17203f947f7038ea567d6f2ed93b5
G4be34 <jimenezgabriel12@gmail.com>
2023-01-15 14:49:31 -0700

    Create calculator object and add operation functions

--- index.js
+++ index.js
@@ -1,5 +1,35 @@
 class Calculator {
-    constructor(prevOutput, curOutput);
+    constructor(prevOutput, curOutput) {
+        this.prevOutput = prevOutput;
+        this.curOutput = curOutput;
+        this.clear();
+    }
+
+ clear() {
+    this.prevOperand = '';
+    this.curOperand = '';
+    this.operation = undefined;
+};
+
+ del() {
+
+};
+
+ addNumber(number) {
+
+};
+
+ getOperation(operation) {
+
+};
+
+ compute() {
+
+};
+
+ updateDisplay() {
+    
+};
 };
 
 const numbers = document.querySelectorAll('.btnNum');
@@ -10,3 +40,11 @@ const equals = document.querySelector('.equalsBtn');
 const prevOutput = document.querySelector('.previous-output');
 const curOutput = document.querySelector('.current-output');
 
+const calculator = new Calculator(prevOutput, curOutput);
+
+numbers.forEach(number => {
+    number.addEventListener('click', () => {
+        calculator.addNumber(number.innerText);
+        calculator.updateDisplay();
+    });
+});
